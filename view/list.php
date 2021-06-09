aeb58e9b1d5f9069ca9a8717b1b301e023ae3108
khai-cotrol <khaidj7398@gmail.com>
2021-06-09 17:22:58 +0700

    them view

---
+++ view/list.php
@@ -0,0 +1,35 @@
+<div class="col-12 col-md-12 mt-2">
+    <div class="card">
+        <div class="card-header">
+            Danh sách san pham
+        </div>
+        <div class="card-body">
+            <div class="col-12">
+                <a class="btn btn-success mb-2" href="index.php?page=product&action=add">Thêm mới</a>
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>STT</th>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Image</th>
+                        <th>Publishing company</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+<!--                    --><?php //foreach (): ?>
+<!--                        <tr>-->
+<!--                            <td>--><?php //echo ?><!--</td>-->
+<!--                            <td>--><?php //echo ?><!--</td>-->
+<!--                            <td>--><?php //echo  ?><!--</td>-->
+<!--                            <td>--><?php //echo  ?><!--</td>-->
+<!--                            <td>--><?php //echo  ?><!--</td>-->
+<!--                            <td></td>-->
+<!--                        </tr>-->
+<!--                    --><?php //endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
